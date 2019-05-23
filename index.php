fefe46ccfc4e56261cf1d65ab9210eb084cff826
Bal2018 <44124657+Bal2018@users.noreply.github.com>
2019-05-23 21:20:56 +0100

    Index

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Roadster Blog: Bootstrapping
+ * 
+ */
+
+// Setup error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
+
+// Load config, tools, services and router
+include_once $_SERVER['DOCUMENT_ROOT'] . "/support/lib/Tools.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/support/lib/NSConfig.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/support/lib/NSRouter.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/support/lib/NSViewManager.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/support/lib/NSServiceProvider.php";
+
+
+// Set up database support libs
+include_once $_SERVER['DOCUMENT_ROOT'] . "/support/lib/NSParam.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/support/lib/NSDatabase.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/support/models/Model.php";
+
+
+// Loads NSResult to allow returning JSON values to browser
+include_once $_SERVER['DOCUMENT_ROOT'] . "/support/lib/NSResult.php";
+
+
+// Bootstrap Repositories
+include_once $_SERVER['DOCUMENT_ROOT'] . "/support/repository/Repository.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/support/repository/ArticleRepository.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/support/repository/UserRepository.php";
+
+
+// Create service provider
+$ServiceProvider = new NSServiceProvider();
+
+// Start Service
+$ServiceProvider->startService();
